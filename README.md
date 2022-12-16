8abaa949256938059d92bb4c2d865b1965269bb8
Chinmay5767 <82484297+Chinmay5767@users.noreply.github.com>
2022-12-17 00:39:58 +0530

    ok

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ A new Flutter project.
 This project is a starting point for a Flutter application.
 ## It has used cloud firestore service
 ## used flutterfire dependencies
+## faced problem in accessing WidgetsFlutterBinding.ensureInitialized();
+ ## await Firebase.initializeApp(); in project. It was not running after this line of code->
+## things which I done to solve problem->
+## first I have to build gradle successfully for that I had to solve error related to google service ->
+## 1) update dependencies
+## 2) update flutter by flutter upgrade
+## 3) change minsdk from 16 to 20 from local.properties file
+## 4) Name of GoogleServices.json file should not be GoogleServices(1).json same for google_servicesInfo.json
+
 
 A few resources to get you started if this is your first Flutter project:
 
--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 class Authentication extends StatefulWidget {
@@ -8,10 +10,80 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
+   
+   // ignore: non_constant_identifier_names
+   final TextEditingController _EmailFeild = TextEditingController();
+   final TextEditingController _PasswordFeild = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(),
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          // ignore: prefer_const_constructors
+          decoration: BoxDecoration(
+            color: Colors.grey,
+          ),
+          child: Flexible(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextFormField(
+                  controller: _EmailFeild,
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    hintText: "abc@email.com",
+                    // ignore: prefer_const_constructors
+                    hintStyle: TextStyle(
+                    color: Colors.black87,
+          
+                    ),
+                    labelText: "Email",
+                    // ignore: prefer_const_constructors
+                    labelStyle: TextStyle(
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                   controller: _PasswordFeild,
+                   obscureText: true,
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    hintText: "Passw",
+                    // ignore: prefer_const_constructors
+                    hintStyle: TextStyle(
+                    color: Colors.black87,
+          
+                    ),
+                    labelText: "Pass",
+                    // ignore: prefer_const_constructors
+                    labelStyle: TextStyle(
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(1.3),
+                    color: Colors.redAccent,
+          
+                  ),
+                  child: MaterialButton(onPressed:() {} , child: Text("Log in")),
+                ),
+                Container(
+                   width: MediaQuery.of(context).size.width / 1.3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(1.3),
+                    color: Colors.redAccent,
+          
+                  ),
+                  child: MaterialButton(onPressed:() {} , child: Text("password")),),
+              ],
+            ),
+          )),
     );
   }
 }
