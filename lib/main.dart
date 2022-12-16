d363da6a2b532e086e2707288035c1b2c4da663a
Chinmay5767 <82484297+Chinmay5767@users.noreply.github.com>
2022-12-17 00:33:52 +0530

    completed ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
